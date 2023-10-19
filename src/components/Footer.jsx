4585bcd787288ea4e1eba8c1746d961cb2c983cd
Aradhana-01 <134721831+Aradhana-01@users.noreply.github.com>
2023-10-19 07:15:46 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+  
+    <div>
+      <h1>TechStar</h1>
+      <p>@All Rights Reserved </p>
+    </div>
+    <div>
+      <h5>Follow Us</h5>
+      <div>
+        <a href="#"  target={"blank"}>Instagram</a>
+        <a href="#"  target={"blank"}>LinkedIn</a>
+        <a href="#"  target={"blank"}>GitHub</a>
+      </div>
+    </div>
+    </footer>
+  )
+}
+
+export default Footer
